485a278d02b383f621707a7af64757973e749924
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-12-24 16:54:24 -0600

    Fix whitespace errors

--- footnotes.php
+++ footnotes.php
@@ -15,37 +15,37 @@ define('WP_FOOTNOTES_OPEN', " ((");
 define('WP_FOOTNOTES_CLOSE', "))");
 define('WP_FOOTNOTES_VERSION', '1.3.1');
 
-// Instantiate the class 
+// Instantiate the class
 $swas_wp_footnotes = new swas_wp_footnotes();
 
 // Encapsulate in a class
 class swas_wp_footnotes {
 	var $current_options;
 	var $default_options;
-	
+
 	/**
 	 * Constructor.
 	 */
-	function swas_wp_footnotes() {		
-	
-		// Define the implemented option styles		
+	function swas_wp_footnotes() {
+
+		// Define the implemented option styles
 		$this->styles = array(
 			'decimal' => '1,2...10',
 			'decimal-leading-zero' => '01, 02...10',
 			'lower-alpha' => 'a,b...j',
 			'upper-alpha' => 'A,B...J',
 			'lower-roman' => 'i,ii...x',
-			'upper-roman' => 'I,II...X', 
+			'upper-roman' => 'I,II...X',
 			'symbol' => 'Symbol'
 		);
-		
+
 		// Hook me up
 		add_action('the_content', array($this, 'process'), 11);
 	}
-	
-	
+
+
 	/**
-	 * Searches the text and extracts footnotes. 
+	 * Searches the text and extracts footnotes.
 	 * Adds the identifier links and creats footnotes list.
 	 * @param $data string The content of the post.
 	 * @return string The new content with footnotes generated.
@@ -55,18 +55,18 @@ class swas_wp_footnotes {
 
 		// Check for and setup the starting number
 		$start_number = (preg_match("|<!\-\-startnum=(\d+)\-\->|",$data,$start_number_array)==1) ? $start_number_array[1] : 1;
-	
+
 		// Regex extraction of all footnotes (or return if there are none)
 		if (!preg_match_all("/(".preg_quote(WP_FOOTNOTES_OPEN)."|<footnote>)(.*)(".preg_quote(WP_FOOTNOTES_CLOSE)."|<\/footnote>)/Us", $data, $identifiers, PREG_SET_ORDER)) {
 			return $data;
 		}
-		
+
 		$display = true;
-		
+
 		$footnotes = array();
-		
+
 		$style = 'decimal';
-		
+
 		// Create 'em
 		for ($i=0; $i<count($identifiers); $i++){
 			// Look for ref: and replace in identifiers array.
@@ -76,7 +76,7 @@ class swas_wp_footnotes {
 			}else{
 				$identifiers[$i]['text'] = $identifiers[$i][2];
 			}
-			
+
 			if (!isset($identifiers[$i]['use_footnote'])){
 				// Add footnote and record the key
 				$identifiers[$i]['use_footnote'] = count($footnotes);
@@ -85,7 +85,7 @@ class swas_wp_footnotes {
 				$footnotes[$identifiers[$i]['use_footnote']]['identifiers'][] = $i;
 			}
 		}
-		
+
 		// Footnotes and identifiers are stored in the array
 
 		$use_full_link = false;
@@ -96,7 +96,7 @@ class swas_wp_footnotes {
 		// Display identifiers
 		$datanote = ''; // Bugfix submitted by Greg Sullivan
 		foreach ($identifiers as $key => $value) {
-				
+
 			$id_num = ($style == 'decimal') ? $value['use_footnote']+$start_number : $this->convert_num($value['use_footnote']+$start_number, $style, count($footnotes));
 			$id_id = "rf".$id_num."-".$post->ID;
 			$id_href = ( ($use_full_link) ? get_permalink($post->ID) : '' ) . "#fn".$id_num."-".$post->ID;
@@ -104,7 +104,7 @@ class swas_wp_footnotes {
 			$id_replace = '<sup id="'.$id_id.'"><a href="'.$id_href.'" title="'.$id_title.'" rel="footnote">'.$id_num.'</a></sup>';
 			if ($display) $data = substr_replace($data, $id_replace, strpos($data,$value[0]),strlen($value[0]));
 			else $data = substr_replace($data, '', strpos($data,$value[0]),strlen($value[0]));
-	
+
 	// Display the footnotes (here is where you can change the output)
 
 		// Create each footnote
@@ -115,8 +115,8 @@ class swas_wp_footnotes {
 			$datanote = $datanote.'&#8617;'; // The backlink character (↩)... &#8626 (↲) is another common one
 			$datanote = $datanote.'</a></p></li>'; // After the footnote
 		}
-		
-			
+
+
 	// Create the footnotes
 		foreach ($footnotes as $key => $value) {
 			$data = $data.'<hr class="footnotes"><ol class="footnotes"'; // Before the footnotes
@@ -124,12 +124,12 @@ class swas_wp_footnotes {
 			$data = $data.'>';
 			$data = $data.$datanote; // Don't change this
 			$data = $data.'</ol>'; // After the footnotes
-		
+
 		return $data;
 
 		}
 	}
-	
+
 	function upgrade_post($data){
 		$data = str_replace('<footnote>',WP_FOOTNOTES_OPEN,$data);
 		$data = str_replace('</footnote>',WP_FOOTNOTES_CLOSE,$data);
